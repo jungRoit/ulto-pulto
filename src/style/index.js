bf6d86819c9dc2397421751d84d1ca375c10f917
jungRoit <roit.github@gmail.com>
2020-02-23 14:10:26 +0545

    initial style setup

---
+++ src/style/index.js
@@ -0,0 +1,2 @@
+import './layout.css';
+import './reset.css';
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import '../../style/index';
 
 const Dashboard = props => (
-  <div>
-    <h1>Home</h1>
+  <div className="container">
+    <div className="header">
+      <h1>Header</h1>
+    </div>
+    <div className="body">
+      <h1>body</h1>
+    </div>
+    <div className="footer">
+      <h1>footer</h1>
+    </div>
   </div>
 );
 
