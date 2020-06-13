5a2e9c1e57f90b1b4fe66c80281888bb92168daf
jungRoit <roit.github@gmail.com>
2020-06-13 15:12:36 +0545

    Updates

---
+++ precache-manifest.51629fce11d1c5a8eb7d97dd3fc0b009.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ea044182f0c118f557467c6bbd7105e",
+    "url": "/ulto-pulto/index.html"
+  },
+  {
+    "revision": "3a847e4550330d8a0db6",
+    "url": "/ulto-pulto/static/css/main.84336b6e.chunk.css"
+  },
+  {
+    "revision": "7c64f3e61ff8a2905fdc",
+    "url": "/ulto-pulto/static/js/2.07ee4567.chunk.js"
+  },
+  {
+    "revision": "3a847e4550330d8a0db6",
+    "url": "/ulto-pulto/static/js/main.00944832.chunk.js"
+  },
+  {
+    "revision": "6821330662621fcf7681",
+    "url": "/ulto-pulto/static/js/runtime-main.646ab136.js"
+  },
+  {
+    "revision": "fc86bbed2bdecfd8ea2c0e3f1cedf608",
+    "url": "/ulto-pulto/static/media/border2.fc86bbed.png"
+  },
+  {
+    "revision": "36f9b2a19bbf43e6b1cb0c778255f2fa",
+    "url": "/ulto-pulto/static/media/cover-1.36f9b2a1.jpg"
+  },
+  {
+    "revision": "a695901b8c1c1a3efbbf649cce613bfa",
+    "url": "/ulto-pulto/static/media/golden.a695901b.png"
+  }
+]);
